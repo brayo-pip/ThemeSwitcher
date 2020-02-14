a569ecaffa1a39222c1068e0266411ce1ba92f4e
MohnishKalia <mohnish.kalia@gmail.com>
2020-02-14 12:04:46 -0600

    Command and time extensibility

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,61 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { resolve, normalize } from 'path';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	const userSettings = vscode.workspace.getConfiguration();
 	const extensionConfig = vscode.workspace.getConfiguration('themeswitcher');
 
-	const lightTheme = userSettings.get('workbench.preferredLightColorTheme');
-	const darkTheme = userSettings.get('workbench.preferredDarkColorTheme');
+	const prefLight = 'workbench.preferredLightColorTheme';
+	const lightTheme = userSettings.get(prefLight);
+	const prefDark = 'workbench.preferredDarkColorTheme';
+	const darkTheme = userSettings.get(prefDark);
 
-	const startDay = extensionConfig.get('startDay') as number;
-	const endDay = extensionConfig.get('endDay') as number;
+	const startDay = extensionConfig.get('startDay') as string;
+	const endDay = extensionConfig.get('endDay') as string;
 
-	const currentHour = new Date().getHours();
-	// const isDay = false;
-	const isDay = currentHour > startDay && currentHour < endDay;
+	context.subscriptions.push(vscode.commands.registerCommand('themeswitcher.changeDay', async () => {
+		const start = await vscode.window.showInputBox({ prompt: 'Enter start day time.', placeHolder: 'HH:MM' });
+		extensionConfig.update('startDay', start, true);
+		const end = await vscode.window.showInputBox({ prompt: 'Enter end day time.', placeHolder: 'HH:MM' });
+		extensionConfig.update('endDay', end, true);
+	}));
+
+	let startTime = 0, endTime = 0;
+
+	try {
+		startTime = getDate(startDay).getTime();
+		endTime = getDate(endDay).getTime();
+	} catch (e) {
+		const cv = 'Change Values';
+		vscode.window.showErrorMessage('Invalid start or end day values.', cv)
+			.then(choice => {
+				if (choice === cv) vscode.commands.executeCommand('themeswitcher.changeDay');
+			});
+		return;
+	}
+
+	const currentTime = new Date().getTime();
+
+	const isDay = currentTime > startTime && currentTime < endTime;
 	const currentTheme = isDay ? lightTheme : darkTheme
 
 	// console.log(lightTheme, darkTheme, startDay, endDay, currentHour, isDay, currentTheme)
 
 	userSettings.update("workbench.colorTheme", currentTheme, true);
 
-	// // Use the console to output diagnostic information (console.log) and errors (console.error)
-	// // This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "themeswitcher" is now active!');
-
-	vscode.window.showInformationMessage(`It is ${isDay ? 'day' : 'night'} time. Your theme is now ${currentTheme}.`);
+	vscode.window.showInformationMessage(`It is ${isDay ? 'day' : 'night'} time. Your theme is now ${currentTheme}.`)
 }
 
-// this method is called when your extension is deactivated
+function getDate(str: string): Date {
+	const hm = str.split(':');
+	console.log(hm);
+	let date = new Date();
+	const hours = Number.parseInt(hm[0]);
+	const mins = Number.parseInt(hm[1]);
+	if (Number.isNaN(hours) || Number.isNaN(mins)) throw new Error('Bad value');
+	date.setHours(hours);
+	date.setMinutes(mins);
+	return date;
+}
+
 export function deactivate() { }
